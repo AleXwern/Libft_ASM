d235ff19211b17f83242903406db8db39593533a
AleX Nyström <alex.nystrom5@gmail.com>
2021-07-18 22:41:29 +0300

    Some small stuff from cottage in side to graphical projects -> trim and sub, also unpushed new

--- src/ft_strlen.asm
+++ src/ft_strlen.asm
@@ -1,16 +1,16 @@
 section	.text
 	global ft_strlen
-	
-ft_strlen:	;Prep. get string from RDI and store pointer in RAX/RBX
-	push	rbx
-	mov		rbx, rdi
+
+;size_t		ft_strlen(const char *str)
+;Receives null terminated string str and returns the length of the string.
+;rdi=str
+ft_strlen:
 	mov		rax, rdi
 getchar:	;Compare character to 0 to see if we are at end. If 0 jump to return.
 	cmp		byte [rax], 0
 	jz		done
 	inc		rax
 	jmp		getchar
-done:		;subtract RBX addr ftom RAX addr to see how many bytes we moved.
-	sub		rax, rbx
-	pop		rbx
+done:		;We get return value by subtracting the offset value by the original value
+	sub		rax, rdi
 	ret
